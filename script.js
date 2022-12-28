80c86e42fdb78568d78dc8a73dad761fa7ef5438
KaungPyaeSoneAung2 <kaungpyae.kpp16@gmail.com>
2022-12-28 19:56:50 +0630

    raw version

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 function liked() {
-  var img = document.getElementById("like-img");
-  if(img.src.match('./assets/heartBlack.svg')){
-    img.src='./assets/heartRed.svg'
+  const img = document.getElementById("like-img");
+  if(img.src.match("./assets/heartBlack.svg")){
+    img.src="./assets/heartBlack.svg"
   }
   else{
-    img.src='./assets/heartBlack.svg'
+    img.src="./assets/heartBlack.svg"
   }
 }
 function playPause() {
-    alert("Yes")
-    var video = document.getElementById("bgvideo")
-    if (video.paused)
-      video.play(); 
-    else 
+    const video = document.getElementById("bgvideo")
+    const icon = document.getElementById("videoPausedorPlay")
+    const music= document.getElementById("music")
+    if (video.paused){
+      video.play();
+      icon.style.visibility="hidden";
+      music.play();
+    }
+    else {
       video.pause();
+      icon.style.visibility="visible";
+      music.pause();
+    }
    }
